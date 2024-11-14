4382bb6342f0aea1a14a68dacf6e143f250797b1
priyansh2000 <raipriyansh20@gmail.com>
2024-11-15 00:47:12 +0530

    Password Hashed using BCrypt

--- src/main/java/com/example/yummy/service/CustomerService.java
+++ src/main/java/com/example/yummy/service/CustomerService.java
@@ -4,6 +4,7 @@ import com.example.yummy.dto.CustomerRequest;
 import com.example.yummy.entity.Customer;
 import com.example.yummy.mapper.CustomerMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.yummy.repo.CustomerRepo;
 import com.example.yummy.dto.CustomerRequest;
@@ -14,9 +15,11 @@ import com.example.yummy.dto.CustomerRequest;
 public class CustomerService {
     private final CustomerRepo repo;
     private final CustomerMapper mapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
+
     public String createCustomer(CustomerRequest request) {
         Customer customer = mapper.toEntity(request);
-
+        customer.setPassword(bCryptPasswordEncoder.encode(customer.getPassword()));
         repo.save(customer);
         return "Created";
     }
